0a103f11f76209e1c5da2468486411e2e56d1a6d
lapp14 <d.lapp14@gmail.com>
2014-06-16 01:26:34 -0400

    Added php files for main menu and footer menu html

---
+++ menu.php
@@ -0,0 +1,32 @@
+
+	<div class="menuContainer">	
+		<div class="nav">
+			<ul id="menu" class="menu">
+				<li class="nodiv"><a href="index.html">About Me</a></li>
+				<li><a href="projects.html">Projects</a>
+					<ul>
+						<li><a href="opengl.html">OpenGL</a>
+							<ul>
+								<li><a href="opengl/pool.html">3D Pool</a></li>
+								<li><a href="opengl/pfountain.html">Particle Fountain</a></li>
+								<li><a href="opengl/gameoflife.html">Game of Life</a></li>
+							</ul>
+						</li>
+						<li><a href="brickbreaker.html">Android</a>
+							<ul>
+								<li><a href="brickbreaker.html">Brick Breaker</a></li>
+							</ul>
+						</li>  
+							<li><a href="webproject.html">Web</a>
+							<ul>
+								<li><a href="webproject.html">Final Project</a></li>
+							</ul>
+						</li>     						
+					</ul>
+				</li>
+				<li class="nodiv"><a href="resume.html">Resume</a></li>
+				<li class="nodiv"><a href="contact.html">Contact</a></li>
+            </ul>
+		</div>
+	</div>
+	
