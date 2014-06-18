9940e96f809e610696bca2b6f2c7d7fdaf35647f
lapp14 <d.lapp14@gmail.com>
2014-06-18 12:18:46 -0400

    Cross browser compatability
    -Compatable with IE7+, Mozilla and webkit browsers
    -fixed document declaration

--- contact.php
+++ contact.php
@@ -1,11 +1,4 @@
-<!--  
-	project/contact.php
-	Dan Lapp
-	COSC 2P89
-	Fall 2013
--->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<link rel="stylesheet" href="css/Style.css" type="text/css" />
